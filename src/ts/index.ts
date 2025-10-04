11b62d27ae13f4be34e219ddfb25c64fded20a16
vincent <1339301794@qq.com>
2025-10-04 22:02:39 +0800

    1

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,7 +1,8 @@
 import GeoWorld from './GeoWorld/GeoWorld';
 
-const dom: HTMLElement = document.querySelector('#earth-canvas')!;
-
-new GeoWorld({
-  dom: dom
-})
+const dom: HTMLElement = document.querySelector('#earth-canvas');
+if(dom){
+  new GeoWorld({
+    dom: dom
+  })
+}
